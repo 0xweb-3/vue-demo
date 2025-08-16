af10ff1fd6325cda2a1b5715e08b898ce10794d2
xin <709464835@qq.com>
2025-08-17 00:03:29 +0800

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,37 @@
-import { createStore } from 'vuex'
+import {createStore, useStore as baseUseStore} from 'vuex'
+import type {Store} from 'vuex'
+import type {InjectionKey} from 'vue'
+import users from './modules/users'
+import checks from './modules/checks'
+import news from "@/store/modules/news";
+import signs from "@/store/modules/signs";
+import type {UsersState} from "./modules/users"
+import type {ChecksState} from "./modules/checks"
+import type {SignsState} from "./modules/signs"
+import type {NewsState} from "./modules/news"
+
+export interface State {
+}
+
+export interface StateAll extends State {
+    users: UsersState
+    checks: ChecksState
+    signs: SignsState
+    news: NewsState
+}
+
+export const key: InjectionKey<Store<StateAll>> = Symbol()
+
+export function useStore() {
+    return baseUseStore(key)
+}
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        users, checks, news, signs
+    }
 })
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import http from "@/utils/http";
 
-http.post("/users/login", {
-  "email": "huangrong@imooc.com",
-  "pass": "huangrong"
-}).then((res) => {
-  console.log(res);
-})
+// http.post("/users/login", {
+//   "email": "huangrong@imooc.com",
+//   "pass": "huangrong"
+// }).then((res) => {
+//   console.log(res);
+// })
+
+import {useStore} from "@/store";
+
+const store = useStore();
+store.dispatch("users/login").then((res)=>{
+  console.log(res.data);
+});
 
 </script>
 
