20c0c26cad3d03a6715d0eb4aef8a725829fd17e
xin <709464835@qq.com>
2025-08-17 00:04:47 +0800

    fix

--- src/store/modules/users.ts
+++ src/store/modules/users.ts
@@ -1,18 +1,19 @@
 import http from '@/utils/http';
-import type { MutationTree, ActionTree, GetterTree } from 'vuex'
-import type { State } from '../index'
+import type {MutationTree, ActionTree, GetterTree} from 'vuex'
+import type {State} from '../index'
 
 export interface UsersState {
 }
 
-const state: UsersState = {
-};
+const state: UsersState = {};
 
-const mutations: MutationTree<UsersState> = {
-};
+const mutations: MutationTree<UsersState> = {};
 const actions: ActionTree<UsersState, State> = {
-    login(){
-        return http.post('/users/login')
+    login() {
+        return http.post('/users/login', {
+            "email": "huangrong@imooc.com",
+            "pass": "huangrong"
+        })
     }
 };
 const getters: GetterTree<UsersState, State> = {};
